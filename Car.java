8726792e9a667f948f6bf1c2e623ae6207ad667e
Muzz16 <elliotlund03@gmail.com>
2024-01-18 13:41:32 +0100

    Added car class

--- Car.java
+++ Car.java
@@ -1,3 +1,16 @@
-public class Car {
+import java.awt.*;
 
+public class Car {
+    private final int nrDoors; // Number of doors on the car
+    private final double enginePower; // Engine power of the car
+    private final double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private final String modelName; // The car model name
+
+    public Car(int nrDoors, double enginePower, double currentSpeed, String modelName){
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.currentSpeed = currentSpeed;
+        this.modelName = modelName;
+    }
 }
