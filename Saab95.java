a5a8ed31ad508bffe6a9244c8c95c15515ecd09c
Muzz16 <elliotlund03@gmail.com>
2024-01-18 14:23:46 +0100

    Added car class

--- Saab95.java
+++ Saab95.java
@@ -23,21 +23,21 @@ public class Saab95 extends Car{
         return enginePower * 0.01 * turbo;
     }
 
-    public void incrementSpeed(double amount){
+    private void incrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() + speedFactor() * amount;
     }
 
-    public void decrementSpeed(double amount){
+     private void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
-    
+
     // TODO fix this method according to lab pm
-    public void gas(double amount){
+    private void gas(double amount){
         incrementSpeed(amount);
     }
 
     // TODO fix this method according to lab pm
-    public void brake(double amount){
+    private void brake(double amount){
         decrementSpeed(amount);
     }
 }
